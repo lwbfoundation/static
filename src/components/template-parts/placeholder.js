fd103479be90342d011db6268fe74dcfe59a1fde
Tobias Butler <tobias@change.org>
2020-10-01 12:28:23 -0700

    fix font weight for firefox

--- src/components/template-parts/placeholder.js
+++ src/components/template-parts/placeholder.js
@@ -5,7 +5,7 @@ import PrelaunchLayout from "../prelaunch-layout";
 
 const Placeholder = ({ data }) => (
   <PrelaunchLayout title={data.wp.generalSettings.title}>
-    <Heading as="h1" textAlign="center" textTransform="uppercase" letterSpacing={4} fontFamily="Trade Gothic, Helvetica">{data.wp.generalSettings.title}</Heading>
+    <Heading as="h1" fontWeight={200} textAlign="center" textTransform="uppercase" letterSpacing={4} fontFamily="Trade Gothic, Helvetica">{data.wp.generalSettings.title}</Heading>
     <PostBody body={data.page.content} />
   </PrelaunchLayout>
 );

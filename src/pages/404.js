4ae15e9bbe0edbee222e965ec997e649115ce5cd
Tobias Butler <tobias@change.org>
2020-10-09 17:00:35 -0700

    add title to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from '@chakra-ui/core';
+import { Helmet } from 'react-helmet';
 import CenteredLayout from '../components/centered-layout';
 
 export default () => (
   <CenteredLayout>
+    <Helmet>
+      <title>Page not found • Lewis W. Butler Foundation</title>
+    </Helmet>
     page not found.{' '}
     <Link href="/" textDecoration="underline">
       go to homepage
